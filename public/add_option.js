4a2b418b06c505df2d2e5904a75b7b06a24aa1ff
xxyzz <elephantpolar@gmail.com>
2017-09-21 21:55:56 +0800

    remove option

--- public/add_option.js
+++ public/add_option.js
@@ -1,5 +1,9 @@
-function addMoreOption() {
-  $(this).parent().before( `<div class="input-field col s12"><input id="options" name="poll[options][]" required="" type="text"><label for="options">Extra Option</label></div>` );
+function addOption() {
+    $(this).parent().before(`<div class="input-field col s12 extra-option"><input class="extra-input" id="options" name="poll[options][]" required="" type="text"><label for="options">Extra Option</label><i class="material-icons remove_option">close</i></div>`);
 };
 
-$('#add_option_button').on('click', addMoreOption);
+function removeOption() {
+    $(this).closest('.extra-option').remove();
+}
+$('#add_option_button').click(addOption);
+$('.form').on('click', '.remove_option', removeOption);
