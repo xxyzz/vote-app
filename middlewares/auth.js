e4534d89c4228afc6c61b88f89891b401680db0c
xxyzz <elephantpolar@gmail.com>
2017-09-24 21:46:28 +0800

    post vote

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,20 @@
+var Poll = require('./../models/poll');
+
 function isAuthenticated(req, res, next) {
-  if (req.isAuthenticated())
-    return next();
-  res.redirect('/login');
+    if (req.isAuthenticated())
+        return next();
+    res.redirect('/login');
 }
-module.exports = { isAuthenticated };
+
+function checkVoted(req, res, next) {
+	isAuthenticated(req, res, function() {
+        Poll.findById(req.params.pollId)
+            .then(poll => {
+                if (poll.userVoted(req.user)) res.redirect('back');
+                else next();
+            })
+            .catch(err => next(err));
+    });
+};
+
+module.exports = { isAuthenticated, checkVoted };
